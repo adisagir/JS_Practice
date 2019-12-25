638a1beb855b2500950c58f56560beb6d77971ce
github username <adisagir@gmail.com>
2019-12-25 14:00:14 -0500

    Completed

---
+++ Basics Part 2/Arrays_Adding_Elements.js
@@ -0,0 +1,9 @@
+// Use a built-in JavaScript method to add another show to the netflixShows array. Then log your updated array to the console.
+
+const netflixShows = ["Orange is the New Black", "Black Mirror", "Chewing Gum"];
+
+// ADD CODE HERE
+netflixShows.push("Narcos");
+
+// Write a console.log statement below to check your work!
+console.log(netflixShows);
