0fab1c0cb86fd51ac8745cfe4812be73007dd2df
github username <adisagir@gmail.com>
2020-01-04 22:15:25 -0500

    Completed

---
+++ Basics Part 2/Arrays_Examining_Elements.js
@@ -0,0 +1,9 @@
+// Create a variable called fourthItem and assign it the value of the fourth item in the horror array ('Ghostface'). Then console.log fourthItem to see the output.
+
+const horror = ["Freddy", "Jason", "Michael", "Ghostface", "Chucky"];
+
+// ADD CODE HERE
+let fourthItem = horror[3];
+
+// Write a console.log statement below to check your work!
+console.log(fourthItem);
